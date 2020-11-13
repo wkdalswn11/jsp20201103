34ddf1256d91cb1043b4462541f042c592a7fd53
wkdalswn11 <wkdalswn133@naver.com>
2020-11-13 17:48:36 +0900

    20201113

---
+++ WebContent/chap14/lecture/select/orderEx1View.jsp
@@ -0,0 +1,63 @@
+<%@page import="chap14.EmployeeDao"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="java.util.*" %>
+<% request.setCharacterEncoding("utf-8"); %>
+<%
+String name = request.getParameter("name");
+String order = request.getParameter("order");
+if (name == null) {
+	name = "";
+}
+name = name.toUpperCase();
+if(order == null) {
+	order = "1";
+}
+
+List<String> list = null;
+
+switch (order) {
+case "1":
+	list = EmployeeDao.getNameLike(name);
+	break;
+case "2":
+	list = EmployeeDao.getNameList(name, true);
+	break;
+case "3":
+	list = EmployeeDao.getNameList(name, false);
+	break;
+}
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<title>Insert title here</title>
+</head>
+<body>
+<h1>검색된 이름</h1>
+<%
+if(list.size() >0) {
+%>
+<ul>
+<%
+	for(String n : list) {
+%>
+	<li><%= n %></li>
+	<%
+	}
+	%>
+<%
+	} else {
+%>
+<h2>검색된 이름이 없습니다</h2>
+<%
+	}
+%>	
+</ul>
+</body>
+</html>
--- src/chap14/EmployeeDao.java
+++ src/chap14/EmployeeDao.java
@@ -9,6 +9,158 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeDao {
+	public static List<String> getNameList(String name, boolean asc) {
+		List<String> list = new ArrayList<>();
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+		String user = "c##mydbms";
+		String password = "admin";
+		String sql = "SELECT ename "
+				+ "FROM employee "
+				+ "WHERE "
+				+ "ename LIKE '%" + name + "%' "
+				+ "ORDER BY ename " ;
+		if(asc) {
+			sql += "ASC"; // 오름차순
+		} else {
+			sql += "DESC"; // 내림차순
+		}
+		try {
+			// 1. 클래스 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			// 2. 커넥션 생성
+			conn = DriverManager.getConnection(url, user, password);
+			
+			// 3. statement 생성
+			stmt = conn.createStatement();
+			
+			// 4. 쿼리 실행
+			rs = stmt.executeQuery(sql);
+			
+			// 5. 결과 처리
+			while(rs.next()) {
+				list.add(rs.getString("ename"));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			
+			try {
+				if (stmt != null) {
+					// 6. statement 닫고
+					stmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			// 7. connection 닫고
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}								
+		return list;
+		
+//		try with source 를 사용한 예제 위와 하는 역할은 같음
+//		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+//		String user = "c##mydbms";
+//		String password = "admin";
+//		// 1. 드라이버 로딩
+//		try {
+//			Class.forName("oracle.jdbc.driver.OracleDriver");
+//		} catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		// 2. 연결
+//		// 3. statment
+//		try (
+//				Connection conn = DriverManager.getConnection(url, user, password);
+//				Statement stmt = conn.createStatement();
+//				) {
+//			// 4. 쿼리
+//			ResultSet rs = stmt.executeQuery(sql);
+//			// 5. 처리
+//			while (rs.next()) {
+//				list.add(rs.getString("ename"));
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+	}
+	
+	public static List<String> getNameLike(String name) {
+		List<String> list = new ArrayList<>();
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		name = name.toUpperCase();
+		
+		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+		String user = "c##mydbms";
+		
+		String password = "admin";
+		
+		String sql = "SELECT ename "
+				+ "FROM employee "
+				+ "WHERE "
+				+ "ename LIKE '%" + name + "%'";
+		try {
+			// 1. 클래스 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			// 2. 커넥션 생성
+			conn = DriverManager.getConnection(url, user, password);
+			
+			// 3. statement 생성
+			stmt = conn.createStatement();
+			
+			// 4. 쿼리 실행
+			rs = stmt.executeQuery(sql);
+			
+			// 5. 결과 처리
+			while(rs.next()) {
+				list.add(rs.getString("ename"));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			
+			try {
+				if (stmt != null) {
+					// 6. statement 닫고
+					stmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			// 7. connection 닫고
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}								
+		return list;
+	}
+	
+	
+	
 	public static List<String> listEmployeeName() {
 		List <String> list = new ArrayList<>();
 		Connection conn = null;
@@ -43,27 +195,24 @@ public class EmployeeDao {
 			e.printStackTrace();
 		} finally {
 			
-			// 6. statement 닫고
 			try {
 				if (stmt != null) {
+					// 6. statement 닫고
 					stmt.close();
 				}
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			// 7. connection 닫고
 			try {
 				if (conn != null) {
+					// 7. connection 닫고
 					conn.close();
 				}
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
-		}
-				
+		}				
 		return list;
 	}
 }
